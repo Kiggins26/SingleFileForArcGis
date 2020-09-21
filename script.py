4e63d06e17d866234cc354fd0b0e71c469af2008
Andrew Kiggins <andrewkiggins2000@gmail.com>
2020-09-21 12:46:09 -0700

    added the csv intake

--- script.py
+++ script.py
@@ -1,4 +1,15 @@
 #this is a single script for Dr.Fatmi
 #this is to be able to run a python script in ArcGis with the given data
+import csv
+def readCSV(filename):
+    with open (data.csv) as f:
+        reader = csv.reader(f)
+        next(reader)
+        rows = []
+        for row in reader:
+            rows.append(row)
+    return rows
+
+
 
 
