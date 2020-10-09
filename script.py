4312ead121784af498a98c4d46fdc6edb53288cb
Andrew Kiggins <andrewkiggins2000@gmail.com>
2020-10-08 21:45:39 -0700

    changed a few things

--- script.py
+++ script.py
@@ -5,7 +5,7 @@ from math import radians, cos, sin, asin, sqrt
 from scipy import stats, sparse
 import numpy
 def readCSV(filename):
-    with open ("data.csv") as f:
+    with open (filename) as f:
         reader = csv.reader(f)
         next(reader)[18];
         rows = []
@@ -14,7 +14,7 @@ def readCSV(filename):
     cleanedInfo = [];
     i = 0;
     for info in rows:
-        cleanedInfo.append([rows[i][18],rows[i][8],rows[i][7]]) #19 speed_km, latt, long 
+        cleanedInfo.append([(60*60*rows[i][3]+60*rows[i][4]+rows[i][5]),rows[i][8],rows[i][7]]) #19 speed_km, latt, long 
         i = i + 1;
     return cleanedInfo;
 
@@ -44,7 +44,7 @@ def GeoLikelihood(R,Z,sigma): #Normal dis
     holder = [];
     for z in Z:
         for r in R:
-            holder.append(z);
+            holder.append(sqrt(z[1]**2 + z[2]**2));
         norm = numpy.linalg.norm(holder - r, ord=1);
         normResults.append(norm);
         holder.clear();
@@ -56,15 +56,25 @@ def GeoLikelihood(R,Z,sigma): #Normal dis
         prob.append(part1 * part2);
     return prob;             
 
-def TopLikelihood(R,Z): #Students’ t-distribution with 20 degrees of freedom
-    for z in Z:
-        for r in R:
-            holder = 0;
-            max(0,holder - 1);
+def TopLikelihood(R,Z,sigma_t, populationmean): #Students’ t-distribution with 20 degrees of freedom
+    prob = []
+    for i in range(1,len(Z)):
+        norm1 = numpy.linalg.norm([Z[i][1],Z[i-1][1]],ord=1);
+        norm2 = numpy.linalg.norm([Z[i][2],Z[i-1][2]],ord=1);
+        totalNorm = norm1 + norm2;
+        D = distanceBetweenTwoPoints(Z[i],Z[i-z]);  #normally would be based on the network distance, but this is based on c.
+        x = max(0, totalNorm/D);
+        n = len(Z); # sample size
+        df = 20; #degrees of freedom says to use 20 in the paper
+        samplemean = 0; #sample mean
+        t = (samplemean - populationmean)/(sigma_t / sqrt(n));
+        #insert dic here
+        holder = 0;
+        prob.append(holder);
+    return prob;
 
 def TemLikelihood(R,Z,speed): #Expontial dis
-    for z = Z:
-        for r in R:
+   secondspeed = speed / 3600;
 
 def combine(pg,pt,pr):
     p = 0;
@@ -74,4 +84,4 @@ CONST_speed = 50; #50 km assumption
 R = []; #Road Vectors, made of the midpoints on the map, so the more work is needed to be optimal.
 Z = []; #GPS point
 sigma_z = 4.07 # meters based on the paper data, but can be calcuted by 1.4826 median(||zt-xti|| great circle)
-
+populationmean = 2134;
