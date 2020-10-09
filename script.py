3ba5734a9e43c816dc61aa40dcc748dee4cf7f54
Andrew Kiggins <andrewkiggins2000@gmail.com>
2020-10-08 18:12:01 -0700

    updates

--- script.py
+++ script.py
@@ -2,6 +2,8 @@
 #this is to be able to run a python script in ArcGis with the given data
 import csv
 from math import radians, cos, sin, asin, sqrt
+from scipy import stats, sparse
+import numpy
 def readCSV(filename):
     with open ("data.csv") as f:
         reader = csv.reader(f)
@@ -38,25 +40,33 @@ def distanceBetweenTwoPoints(pointOne,pointTwo):
 
 def GeoLikelihood(R,Z,sigma): #Normal dis
     smallestDistance = []
+    r = R;
     for z in Z:
         small = 99999999999999;
-        for r in R:
-            if abs(z-r) < small:
-                small = abs(z-r)
+        if abs(z-r) < small:
+            small = abs(z-r)
         smallestDistance.append(abs(z-small));
-    #normResults = []
-    #for x in smallestDistance:
+    normResults = []
+    for x in smallestDistance:
+
     return smallestDistance;
 
 def TopLikelihood(R,Z): #Students’ t-distribution with 20 degrees of freedom
-
+    for z in Z:
+        for r in R:
+            holder = 0;
+            max(0,holder - 1);
 
 def TemLikelihood(R,Z,speed): #Expontial dis
+    for z = Z:
+        for r in R:
 
-
+def combine(pg,pt,pr):
+    p = 0;
+    return p;
 
 CONST_speed = 50; #50 km assumption
 R = []; #Road Vectors
 Z = []; #GPS point
-
+sigma_z = 4.07 # meters based on the paper data, but can be calcuted by 1.4826 median(||zt-xti|| great circle)
 
